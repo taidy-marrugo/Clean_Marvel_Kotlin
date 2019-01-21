9a7e349f3df7de70e1c49b87fc3099cd379471e6
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-21 17:56:19 -0500

    update observable to fromCallable

--- app/src/main/java/com/puzzlebench/clean_marvel_kotlin/data/service/CharacterServicesImpl.kt
+++ app/src/main/java/com/puzzlebench/clean_marvel_kotlin/data/service/CharacterServicesImpl.kt
@@ -1,37 +1,30 @@
 package com.puzzlebench.clean_marvel_kotlin.data.service
 
+import android.telecom.Call
 import com.puzzlebench.clean_marvel_kotlin.data.mapper.CharacterMapperService
 import com.puzzlebench.clean_marvel_kotlin.data.service.api.MarvelApi
+import com.puzzlebench.clean_marvel_kotlin.data.service.response.CharacterResponse
 import com.puzzlebench.clean_marvel_kotlin.domain.model.Character
 import io.reactivex.Observable
+import java.util.concurrent.Callable
 
 
 class CharacterServicesImpl(private val api: MarvelResquestGenerator = MarvelResquestGenerator(), private val mapper: CharacterMapperService = CharacterMapperService()) {
 
 
     fun getCaracters(): Observable<List<Character>> {
-        return Observable.create { subscriber ->
-            val callResponse = api.createService(MarvelApi::class.java).getCharacter()
-            val response = callResponse.execute()
-            if (response.isSuccessful) {
-                subscriber.onNext(mapper.transform(response.body()?.data!!.characters))
-                subscriber.onComplete()
-            } else {
-                subscriber.onError(Throwable(response.message()))
-            }
-        }
+            return Observable.fromCallable(Callable<List<Character>>() {
+                val callResponse = api.createService(MarvelApi::class.java).getCharacter()
+                val response = callResponse.execute()
+                return@Callable  mapper.transform(response.body()?.data!!.characters)
+            });
     }
 
     fun getCaractersDetail(id: Int?): Observable<List<Character>> {
-        return Observable.create { subscriber ->
+        return Observable.fromCallable(Callable<List<Character>>() {
             val callResponse = api.createService(MarvelApi::class.java).getCharacterDetail(id)
             val response = callResponse.execute()
-            if (response.isSuccessful) {
-                subscriber.onNext(mapper.transform(response.body()?.data!!.characters))
-                subscriber.onComplete()
-            } else {
-                subscriber.onError(Throwable(response.message()))
-            }
-        }
+            return@Callable  mapper.transform(response.body()?.data!!.characters)
+        });
     }
 }
